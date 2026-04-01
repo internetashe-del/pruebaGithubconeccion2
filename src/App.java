1a8f29ab138dece4d3c48703ec7b95f5f9c9aeed
internetashe-del <internetashe@gmail.com>
2026-04-01 17:57:35 -0500

    CONFIRMACION DEL NOMBRE

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
 
     //internetASHE
     //Axel Rincón
+    //Nombre recibido
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
