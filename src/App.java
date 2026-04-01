a5902afcbe859356e6290f9c9265f58f14b7f034
internetashe-del <internetashe@gmail.com>
2026-04-01 17:59:44 -0500

    intentoconfirmacion

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
 
     //internetASHE
     //Axel Rincón
-    //Nombre recibido
+    //Nombre recibido por el admin
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
